6b0fa9ebc32829cecef9f8aeb3391157a13b9ed3
mchyunlixR <maricielochc03@gmail.com>
2025-08-15 22:17:29 -0500

    skzzzzzzzzzzzzzzzzzzzz

---
+++ src/main/java/pe/idat/kafka_productor/controller/MailController.java
@@ -0,0 +1,42 @@
+package pe.idat.kafka_productor.controller;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.RestController;
+import pe.idat.kafka_productor.service.ProducerMailService;
+
+
+@RestController
+
+@RequestMapping("/api/v1/mail")
+
+public class MailController {
+
+    private final ProducerMailService producerMailService;
+
+    public MailController(ProducerMailService producerMailService) {
+
+        this.producerMailService = producerMailService;
+
+    }
+
+    @GetMapping
+
+    public ResponseEntity<String> enviarEmail(
+
+            @RequestParam String email){
+
+        producerMailService.enviarMail(email);
+
+        return ResponseEntity.ok("Mensaje enviado a Kafka");
+
+    }
+
+}
+
---
+++ src/main/java/pe/idat/kafka_productor/service/ProducerMailService.java
@@ -0,0 +1,31 @@
+package pe.idat.kafka_productor.service;
+
+
+
+import org.springframework.kafka.core.KafkaTemplate;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class ProducerMailService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    private static final String TOPIC = "idat-topic";
+
+    public ProducerMailService(
+
+            KafkaTemplate<String, String> kafkaTemplate) {
+
+        this.kafkaTemplate = kafkaTemplate;
+
+    }
+
+    public void enviarMail(String email){
+
+        kafkaTemplate.send(TOPIC, email);
+
+    }
+
+}
